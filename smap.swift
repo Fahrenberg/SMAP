1694f6f9cabfe0cc0968703829047f14648b5a89
Jean-Nicolas <support@fahrenberg.ch>
2021-12-18 23:00:50 +0100

    Initial Release 1.0.0

---
+++ smap.swift
@@ -0,0 +1,64 @@
+//
+//  main.swift
+//  smap
+//
+//  Created by Jean-Nicolas on 17.12.21.
+//
+
+import Foundation
+import ArgumentParser
+import AppKit
+
+
+
+@main
+struct smap: ParsableCommand {
+    enum smapError: Error {
+        case FileDoesNotExists(_: String)
+        case CannotReadData(fromFile: String)
+        case CannotCreateLink
+    }
+    
+    
+    @Flag(name: .shortAndLong, help: "Show Location as Map in Browser")
+    var browser = false
+    
+    @Argument(help: "Image file path")
+    var imageFilePath: String
+    
+    mutating func run() throws  {
+        let manager = FileManager.default
+
+        guard manager.fileExists(atPath: imageFilePath) else {
+            throw smapError.FileDoesNotExists(imageFilePath)
+        }
+
+        let imageFileURL = URL(fileURLWithPath: imageFilePath)
+        guard let data = try? Data(contentsOf: imageFileURL) else {
+            throw smapError.CannotReadData(fromFile: imageFilePath)
+        }
+
+        guard let mapLink = ImageMetadata(data: data)?.mapURL else {
+            print("Cannot create link from data")
+            throw smapError.CannotCreateLink
+            
+        }
+        
+        if browser {
+            NSWorkspace.shared.open(mapLink)
+        } else {
+            print("\"" + mapLink.absoluteString + "\"")
+        }
+        
+
+        
+       
+        
+    }
+    
+}
+
+
+
+
+
---
+++ smap/ImageMetadata.swift
@@ -0,0 +1,77 @@
+//
+//  ImageMetadata.swift
+//  ImageMetaDataReader
+//
+//  Created by Jean-Nicolas on 15.12.21.
+//
+
+import Foundation
+import LocationKit
+import MapKit
+
+public struct ImageMetadata {
+    typealias PropertiesDict = [CFString:Any]
+    
+    var properties : PropertiesDict
+    
+    init?(url: URL) {
+        guard let data = try? Data(contentsOf: url) else {
+                return nil
+        }
+        self.init(data: data)
+    }
+    
+    
+    init?(data: Data) {
+        let options = [kCGImageSourceShouldCache: kCFBooleanFalse]
+        if let imageSource = CGImageSourceCreateWithData(data as CFData, options as CFDictionary),
+           let imageProperties = CGImageSourceCopyPropertiesAtIndex(imageSource, 0, nil) as? PropertiesDict {
+            self.properties = imageProperties
+        } else {
+            return nil
+        }
+    }
+    
+    var coordinate: CLLocationCoordinate2D? {
+        guard let gpsMetadata = self.properties[kCGImagePropertyGPSDictionary] as? PropertiesDict else {
+            return nil
+        }
+        guard let lat = gpsMetadata[kCGImagePropertyGPSLatitude] as? Double,
+              let latRef = gpsMetadata[kCGImagePropertyGPSLatitudeRef] as? String,
+              let lng = gpsMetadata[kCGImagePropertyGPSLongitude] as? Double,
+              let lngRef = gpsMetadata[kCGImagePropertyGPSLongitudeRef] as? String else {
+            print("FotoProperties - coordinate \(Properties.noCoordinate)")
+            return nil
+        }
+        let wgs84 =  "\(lat)\(CoordinateAccents.degreeAccent.rawValue)\(latRef) \(lng)\(CoordinateAccents.degreeAccent.rawValue)\(lngRef)"
+        guard let parsedCoordinate = wgs84.parseToCoordinate2D else {
+            print("FotoProperties - parsedCoordinate == nil: \(Properties.noCoordinate)")
+            return nil
+        }
+        return parsedCoordinate
+    }
+    
+    var mapURL: URL? {
+        guard let mapCoordinate = self.coordinate else {
+            return nil
+        }
+        var swissTopoComponent = URLComponents(string: "https://map.geo.admin.ch/")
+        let coordinateString = "\(mapCoordinate.latitude),\(mapCoordinate.longitude)"
+        swissTopoComponent?.queryItems = [URLQueryItem(name: "swisssearch", value: coordinateString)]
+        
+        return swissTopoComponent?.url
+    }
+    
+}
+
+/// Error Codes for Property Parser
+enum Properties: String {
+    case noTIFF = "No TIFF"
+    case noCamera = "No Camera"
+    case noModel = "No Model"
+    case noExif = "No Exif"
+    case noDate = "No DateTimeOriginal"
+    case noDateOffset = "No OffsetTimeOriginal"
+    case noCoordinate = "No Coordinate"
+    case noGPSAltitude = "No Stored GPS-Altitude"
+}
